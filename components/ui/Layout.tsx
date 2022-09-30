63e93f190c28910f762bfa238199a8499d98750c
Eric Mabo <ericmabo@gmail.com>
2022-09-29 23:47:21 -0400

    removed GtAG

--- components/ui/Layout.tsx
+++ components/ui/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Footer from './Footer';
 import Header from './Header';
+import Script from 'next/script';
 
 type Props = {
   children: React.ReactNode;
@@ -14,6 +15,19 @@ const Layout = ({ children }: Props) => {
         <title>SlimAtlanta by ReGenesisDoc LLC </title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
+       {/* <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+   
+      
+      <Script strategy="lazyOnload" id="google-script">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script> */}
       <div className="appflex">
         <Header />
          <div className="main">{children}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script';
 import Layout from '../components/ui/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-   
-      <Script strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout> 
